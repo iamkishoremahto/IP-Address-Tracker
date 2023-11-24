5a2672d1308be05c83a497df86460ee32e3377a5
iamkishoremahto <iamkishoremahto@gmail.com>
2023-11-24 14:49:22 +0530

    git init

--- js/main.js
+++ js/main.js
@@ -2,7 +2,20 @@
 
 var map = L.map('map')
 
-const getIpInfo = async (ip_address = '192.212.174.101') =>{
+var myIcon = L.icon({
+    iconUrl: 'images/icon-location.svg',
+    iconSize:[38, 55],
+})
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 50,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker;
+
+
+const getIpInfo = async (ip_address = '192.212.174.101') => {
     const APIKey = 'at_LJRNbaX3nQwN124gwI8sdc11DCVvV'
 
     // const URL = `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${APIKey}=${ip_address}`
@@ -14,42 +27,32 @@ const getIpInfo = async (ip_address = '192.212.174.101') =>{
 }
 
 
-async function setMap (lat,long){
-    await map.setView([lat,long], 13);
 
-    const customIcon = await L.icon({
-        iconUrl: "/images/icon-location.svg",
-        iconSize:[60,70]
-    })
 
-    await L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+const leafMapHandler = async (pos) => {
+    const lat = pos.lat;
+    const lng = pos.lng;
+    map.setView([lat, lng], 50)
 
-    await L.marker([lat,long],{
-        title:"test",
-        icon: customIcon
-    }).addTo(map);  
+    if (marker) {
+        map.removeLayer(marker);
+    }
+
+    marker = L.marker([lat, lng], {icon: myIcon}).addTo(map);
+    // map.fitBounds(marker.getBounds());
+    map.setView([lat, lng], 13)
+
 }
 
-const leafMapHandler = async (lat,long) => {
 
-    await setMap(lat,long)
 
-    
-}
+const showMap = async (ip_address) => {
+    const ipInfo = await getIpInfo(ip_address)
+    console.log(ipInfo)
 
-async function generateMap(){
-    
-}
-
-const showMap = async (ip_address) =>{
-    // const ipInfo = await getIpInfo()
- 
     const lat = await ipInfo.location.lat
-    const long = await ipInfo.location.lng
-   
+    const lng = await ipInfo.location.lng
+    
     const ip = document.querySelector('#ip');
     const location = document.querySelector('#location');
     const timezone = document.querySelector('#timezone');
@@ -59,41 +62,41 @@ const showMap = async (ip_address) =>{
     location.innerHTML = ipInfo.location.city + ',' + ipInfo.location.region + ',' + ipInfo.location.country + ' ' + ipInfo.location.postalCode
     timezone.innerHTML = ipInfo.location.timezone;
     isp.innerHTML = ipInfo.isp
-
+    leafMapHandler({ 'lat': lat, 'lng': lng })
     // searchBtnHandler(map)
-    return  {'lat':lat, 'lon':long}
+    
 
 }
 
-const searchBtnHandler = async () =>{
-    
+const searchBtnHandler = async () => {
+
     const searchBtn = document.querySelector('#searchBtn');
 
-    searchBtn.addEventListener('click',async () => {
-        const ipInput = document.querySelector('#ip');
-        
+    searchBtn.addEventListener('click', async () => {
+        const ipInput = document.querySelector('#ipInput');
 
-        if(ipInput.value !== '' && ipInput.value !== ' ' && ipInput.value !== null){
+
+        if (ipInput.value !== '' && ipInput.value !== ' ' && ipInput.value !== null) {
             console.log('iam ')
             console.log(ipInput);
-            const latLong = await showMap(ipInput.value);
-        const lat = await  latLong.lat;
-        const lon = await  latLong.lon;
-        console.log(lat, lon);
-         await leafMapHandler(lat, lon);
+            await showMap(ipInput.value);
+            
         }
     })
 }
 
-const showFirstTimeMap = async () =>{
-    const latLong = await showMap();
-    console.log(latLong)
-    leafMapHandler(latLong.lat,latLong.lon);
-}
+// const showFirstTimeMap = async () =>{
+//     const latLong = await showMap();
+//     console.log(latLong)
+//     leafMapHandler(latLong.lat,latLong.lon);
+// }
 
 // showFirstTimeMap()
 // searchBtnHandler();
 
 
 // leafMapHandler(34.04915,-118.09462);
+
+showMap()
+searchBtnHandler()
 
